78f7438a89659ef37bea90c02f946a9fedba86db
SamKopansky <sak162@pitt.edu>
2016-09-25 15:43:55 -0400

    added dropdown to choose dorm

--- app.py
+++ app.py
@@ -5,21 +5,33 @@ from flask import Flask, request, session, redirect, url_for, render_template, j
 
 
 app = Flask(__name__)
-
+host = '0.0.0.0'
 port = int(os.environ.get('PORT', 33507))
+# host = '127.0.0.1'
+# port = 5000
+
+
 
 
 @app.route('/')
 def start_up():
-	return jsonify({'data':get_laundry_data()})
+	return render_template('index.html')
 
 
 
-def get_laundry_data():
+@app.route('/get-raw-data', methods=['POST'])
+def get_raw_data():
+	dorm_name = request.form['dorm-name']
+	raw_laundry_data = get_laundry_data(dorm_name)
+	return jsonify(raw_laundry_data)
+
+
+
+def get_laundry_data(dorm_name):
 	laundry = LaundryAPI()
-	return laundry.get_status_detailed(building_name='TOWERS')
+	return laundry.get_status_detailed(building_name=dorm_name)
 
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=port)
+	app.run(debug=True, host=host, port=port)
