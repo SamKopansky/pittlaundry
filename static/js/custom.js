41cb7c4963dbb2395eabaa83cbdba1aed0e36038
SamKopansky <sak162@pitt.edu>
2016-10-22 18:14:37 -0400

    added machine images to form output

--- static/js/custom.js
+++ static/js/custom.js
@@ -14,18 +14,21 @@ $(document).ready(function() {
         machines.forEach(function(item, index) {
             machineJSON = JSON.stringify(item)
             machine = JSON.parse(machineJSON)
+            status = machine.machine_status
+            timeLeft = machine.time_left
+            
             $('#data-zone').append('<div>')
-            if (machine.machine_status == "Free" && machine.time_left == "0") {
-                $('#data-zone').append('<img src="../static/images/green_machine.png"/>' + '<p>' + machineJSON + '</p>')
+            if (status == "Free" && timeLeft == "0") {
+                $('#data-zone').append('<p><img src="../static/images/green_machine.png"/>Free</p>')
             }
-            else if (machine.machine_status == "Free" && machine.time_left != "0") {
-                $('#data-zone').append('<img src="../static/images/white_machine.png"/>' + '<p>' + machineJSON + '</p>')
+            else if ((status == "Free" && timeLeft != "0") || (status == "In use" && timeLeft == "0")) {
+                $('#data-zone').append('<p><img src="../static/images/white_machine.png"/>Done, but clothes are still in the machine</p>')
             }
-            else if (machine.machine_status == "Out of service") {
-                $('#data-zone').append('<img src="../static/images/black_machine.png"/>' + '<p>' + machineJSON + '</p>')
+            else if (status == "Out of service") {
+                $('#data-zone').append('<p><img src="../static/images/black_machine.png"/>Broken</p>')
             }
-            else if (machine.machine_status == "In use") {
-                $('#data-zone').append('<img src="../static/images/red_machine.png"/>' + '<p>' + machineJSON + '</p>')
+            else if (status == "In use") {
+                $('#data-zone').append('<p><img src="../static/images/red_machine.png"/>Time left: ' + timeLeft + ' minutes</p>')
             }
             $('#data-zone').append('</div>')
             
