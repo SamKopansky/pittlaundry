2eb99c272e010de11d0935866be85e0f4e192eff
SamKopansky <sak162@pitt.edu>
2016-10-22 17:08:12 -0400

    added new machine images

---
+++ static/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#dorm-select').submit(function(e) {
+        e.preventDefault()
+        dorm = $(this).serialize()
+        console.log(dorm)
+        $('#data-zone').html('<img src="static/images/ring.gif"/>')
+        $.post('/get-raw-data', dorm, function(data) {
+            $('#data-zone').empty()
+            displayMachines(data)
+        })
+    })
+    
+    function displayMachines(machines) {
+        machines.forEach(function(item, index) {
+            machine = JSON.parse(item)
+            $('#data-zone').append('<div')
+            if (machine.machine_status.equals("Free")) {
+                $('#data-zone').append('<img src="../static/images/original.png"/>' + '<p>' + JSON.stringify(item) + '</p>')
+            }
+            
+        })
+    }
+})
